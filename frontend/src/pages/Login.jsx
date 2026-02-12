e8ab768201cb35712d1ab54945b9e5f0d3be872b
spencerlogin <spencerlogin03@gmail.com>
2026-02-12 16:07:47 -0500

    Create login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import Navbar from '../components/Navbar.jsx'
+import '../styles/Login.css'
+
+function Login() {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+        const res = await fetch('http://localhost:5050/api/login', { method: 'POST', body: formData, credentials: 'include' })
+        const data = await res.json()
+        if(res.ok) {
+            alert('Success!')
+        }else {
+            alert(data['message'])
+        }
+    }
+
+    return (
+        <>
+            <Navbar />
+            <main>
+                <form className='login-form' onSubmit={handleSubmit}>
+                    <label htmlFor='username-field'>Email or Username</label>
+                    <input id='username-field' type='text' name='username'/>
+                    <label htmlFor='password-field'>Password</label>
+                    <input id='password-field' type='password' name='password'/>
+                    <button type='submit'>Log In</button>
+                </form>
+            </main> 
+        </>
+    )
+}
+
+export default Login
