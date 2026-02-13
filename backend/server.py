0cdd4afbbbe89c90550622a2f8a199a52a3cf43c
Spencer <spencerlogin03@gmail.com>
2026-02-13 02:35:26 -0500

    Signup endpoint and route

--- backend/server.py
+++ backend/server.py
@@ -18,6 +18,59 @@ CORS(app, resources=r'/api/*', allow_headers=['Content-Type'], supports_credenti
 def hello_world():
     return {'res': 'Hello, World!'}
 
+@app.route('/api/signup', methods=['GET', 'POST'])
+def signup_user():
+    email = request.form['email']
+    username = request.form['username']
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    password = request.form['password']
+    
+    # check that username and email are free
+    cnx = get_db_connection()
+    cursor = cnx.cursor()
+    query = (
+        "SELECT username, email FROM users WHERE username=%s OR email=%s"
+    )
+    data = (username, email)
+    userTaken, emailTaken = False, False
+    # TODO: Fix this, users are able to sign up with other users' emails and usernames
+    try:
+        for takenUsername, takenEmail in cursor:
+            if username == takenUsername:
+                userTaken = True
+            if email == takenEmail:
+                emailTaken = True
+    except Exception:
+        # no cursor meaning neither email nor username was taken
+        pass
+
+    # return error if username or email is taken with appropriate error messages
+    if userTaken or emailTaken:
+        cursor.close()
+        cnx.close()
+        if userTaken and emailTaken:
+            return {'message': 'Username and email taken'}, 400
+        elif userTaken:
+            return {'message': 'Username taken'}, 400
+        elif emailTaken:
+            return {'message': 'Email taken'}, 400
+    # insert user into db and generate token
+    else:
+        ph = PasswordHasher.from_parameters(RFC_9106_HIGH_MEMORY) 
+        pwHash = ph.hash(password)
+        token = sha256(str(uuid4()).encode('utf-8')).hexdigest()
+        query = (
+            "INSERT INTO users (firstName, lastName, email, username, password, token)"
+            "VALUES (%s, %s, %s, %s, %s, %s)"
+        )
+        data = (firstName, lastName, email, username, pwHash, token)
+        cursor.execute(query, data)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        return ({'message': 'Success!'}, 200, {'Set-Cookie': f'token={token}; SameSite=Strict; Max-Age=604800; HttpOnly'})
+
 @app.route('/api/login', methods=['GET', 'POST'])
 def login_user():
     username = request.form['username']
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router';
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import Splash from './pages/Splash.jsx'
-import Login from './pages/Login.jsx';
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<Splash />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
