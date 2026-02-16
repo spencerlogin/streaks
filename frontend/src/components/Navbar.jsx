e06ef0ba8309e0a1c127d7e17a5734aea8153809
spencerlogin <spencerlogin03@gmail.com>
2026-02-16 10:24:24 -0500

    Extract Navbar to component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,36 @@
 import { NavLink } from 'react-router';
+import '../styles/Navbar.css'
 
-function Navbar({ action }) {
-    return (
-      <div className='navbar'>
-        <div className='navgroup'>
-          <NavLink to='/' end>
-            <h1>Streaks</h1>
-          </NavLink>
-        </div>
-        <div className='navgroup'>
-          <label className='switch'>
-            <input type='checkbox' onClick={action}/>
-            <span className='slider round'></span>
-          </label>
-          <NavLink to='/login'>
-            <h2>Login</h2>
-          </NavLink>
-          <NavLink to='/signup'>
-            <h2>Signup</h2>
-          </NavLink>
-        </div>
+function Navbar({ action, loggedIn, logout }) {
+
+  return (
+    <div className='navbar'>
+      <div className='navgroup'>
+        <NavLink to='/'>
+          <h1>Streaks</h1>
+        </NavLink>
       </div>
-    )
+      <div className='navgroup'>
+        <label className='switch'>
+          <input type='checkbox' onClick={action} />
+          <span className='slider round'></span>
+        </label>
+
+        {loggedIn &&
+          <button id='logout' onClick={logout}><h2>Logout</h2></button>
+        }
+        {!loggedIn &&
+          <>
+            <NavLink to='/login'>
+              <h2>Login</h2>
+            </NavLink>
+            <NavLink to='/signup'>
+              <h2>Signup</h2>
+            </NavLink>
+          </>}
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
