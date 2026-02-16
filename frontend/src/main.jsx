63436fca4ff7cd83a00fb85b04d0eab1c4255968
spencerlogin <spencerlogin03@gmail.com>
2026-02-16 10:21:50 -0500

    Replace home page
    - displays splash page or dashboard depending on if user logged in
    - handles auth state and routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
-import Splash from './pages/Splash.jsx'
+import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import './index.css'
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Splash />} />
+        <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
